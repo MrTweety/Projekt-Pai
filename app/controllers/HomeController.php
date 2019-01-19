8259d4f54444b3f50173e813a4e1a479cfe7cfe7
Mateusz Gaczorek <kiurshi@gmail.com>
2019-01-19 16:28:29 +0100

    error page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -38,7 +38,37 @@ class HomeController extends Controller
 
 
 
+    public function error404()
+    {
+        $this->partial("header");
+        $user = $this->model('User');
+        if (!$user->is_logged_in()) {
+            $this->partial("nav");
+        } else {
 
+            switch ($user->get_type()) {
+
+                case User::$ADMIN_TYPE:
+                    $dataP=$user->get_name();
+                    $this->partial("nav_admin",$dataP);
+                    break;
+                case '2':
+                    $dataP=$user->get_name();
+                    $this->partial("nav_user",$dataP);
+                    break;
+            }
+
+
+        }
+        $this->view('home/error-404');
+        $this->partial("footer");
+
+    }
+
+    public function error500()
+    {
+        $this->view('home/error-500');
+    }
 
 
 
---
+++ app/views/home/error-404.php
@@ -0,0 +1,45 @@
+
+<style>
+
+    #nopage{
+        padding-top: 100px;
+        margin-top: 66px;
+        background-color: rgba(0,0,0,0.2);
+        text-align: center;
+    }
+
+    #error-box h1{
+
+        margin-top: 40px;
+        color: #605f5f;
+        font-family: 'Titillium Web', Geneva, sans-serif;
+        font-size: 36px !important;
+    }
+
+    #error-box p{
+
+        color: #605f5f;
+        font-family: 'Titillium Web', Geneva, sans-serif;
+        font-size: 15px !important;
+    }
+
+    #img404{
+        width: 218px;
+    }
+</style>
+
+<section id="nopage">
+    <div id="error-box" style="min-height: 900px;">
+        <div>
+            <img id="img404" src="/public/img/error/error-404-v2.png"/>
+        </div>
+        <h1>ERROR 404</h1>
+        <p>
+            Page not found.<br/>
+            If you find this page, let us know.
+        </p>
+        <p>
+            Go back to <a href="/">homepage</a>.
+        </p>
+    </div>
+</section>
